34e718b85a28304549e039e82f781ee164d6d166
Chezlin Benson <102852227+chezlinbenson@users.noreply.github.com>
2023-06-17 23:18:03 +0200

    Revert "#1 - Homepage setup - Chezlin Benson"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import logo from './logo.png';
+import './App.css';
 
-const App = () => (
-    <Routes>
-        <Route path="/" element={<Home />} />
-    </Routes>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          CAPE MUSIC ACADEMY
+        </p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
