9909a74c4e0ff1a24f4dc13e68f16412154e03bc
chandredewet <chandredewet@gmail.com>
2023-06-27 11:39:51 +0200

    it.about-container h2 {s completed

--- src/components/About.js
+++ src/components/About.js
@@ -15,27 +15,30 @@ const About = () => {
             className="about-hero-img"fluid />
         </section>
         <section>
-            <div>
-                <h2>Unlock your musical potential</h2>
-                <p>Whether you're passionate about vocals or instrumental performance, our academy offers a diverse range of departments to suit your interests. From guitar, bass, drums, and piano to our esteemed vocal program, our expert instructors provide comprehensive lessons designed to build your skills from the ground up. You'll learn everything from ear training and sight-reading to improvisation, teamwork, social skills, and live performance techniques.
-                </p>
-            </div>
-        </section>
-        <section>
-            <Image src={pic2} alt="Venue of music academy" fluid />
-            <div>
-                <h2>About Cape Music Academy</h2>
-                <p>Cape Music Academy challenges students to attain their full potential as performers and creative entrepreneurs while establishing and promoting the best standards for music career progression.
-                </p>
-            </div>
-        </section>
-        <section>
-            <Image src={pic3} alt="group of students " fluid />
-            <div>
-                <h2>Join us if you are?</h2>
-                <p>Apply today if you are an Unemployed Youth, a school leaver, an entrepreneur, a musician who need their prior learning to be recognized or if you are eager to broaden their knowledge to gain formal experience or recognize industry related training.
-                </p>
-            </div>
+            <article class="about-article-container">
+                <Image src={pic1} alt="man with guitar" fluid /> 
+                <div>
+                    <h2>Unlock your musical potential</h2>
+                    <p>Whether you're passionate about vocals or instrumental performance, our academy offers a diverse range of departments to suit your interests. From guitar, bass, drums, and piano to our esteemed vocal program, our expert instructors provide comprehensive lessons designed to build your skills from the ground up. You'll learn everything from ear training and sight-reading to improvisation, teamwork, social skills, and live performance techniques.
+                    </p>
+                </div>
+            </article>
+            <article>
+                <Image src={pic2} alt="Venue of music academy" fluid />
+                <div>
+                    <h2>About Cape Music Academy</h2>
+                    <p>Cape Music Academy challenges students to attain their full potential as performers and creative entrepreneurs while establishing and promoting the best standards for music career progression.
+                    </p>
+                </div>
+            </article>
+            <article>
+                <Image src={pic3} alt="group of students " fluid />
+                <div>
+                    <h2>Join us if you are?</h2>
+                    <p>Apply today if you are an Unemployed Youth, a school leaver, an entrepreneur, a musician who need their prior learning to be recognized or if you are eager to broaden their knowledge to gain formal experience or recognize industry related training.
+                    </p>
+                </div>
+            </article>
         </section>
 
     </Container>
